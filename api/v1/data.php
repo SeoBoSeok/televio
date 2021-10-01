2b089f4418374fe5a00567ba6ee16ff67e0307cb
SeoBoSeok <ggy7bbo@gmail.com>
2021-10-01 13:32:01 +0900

    hot fix

--- api/v1/data.php
+++ api/v1/data.php
@@ -7,22 +7,27 @@ include_once(G5_LIB_PATH.'/mailer.lib.php');
 // echo '1111';
 // print_r( $_POST);
 
-$json = $_POST['data'];
+$json = $_POST;
 // // echo $json;
 
 $return = array();
 
-if (!empty($json)) {
-  $return["result"] = "success";
-  $return["data"] = $json;
+// if (!empty($json)) {
+//   $return["result"] = "success";
+//   $return["data"] = $json;
   
-  echo json_encode( $return );
-} else {
+//   echo json_encode( $return );
+// } else {
   
-  $return["result"] = "fail";
+//   $return["result"] = "fail";
 
-  echo json_encode ( $return );
-}
+//   echo json_encode ( $return );
+// }
+
+$return["result"] = "success";
+$return["data"] = $json;
+
+echo json_encode( $return );
 
 
 // curl -d '{ "createAt":"2021-10-01_12-35-16.233355", "id":"id", "shape":"PlateShape.halfRoundRect", "width":130.0, "height":30.0, "title":"010.9792.1006", "titleSize":30.0, "subtitle":"PLEASE CONTACT THIS NUMBER", "subtitleSize":12.0, "plateSubtitlePosition":"PlateSubtitlePosition.top", "fontTitle":"Noto Sans", "fontSubtitle":"Noto Sans", "textColor”:”ffffff”, "backgroundColor”:”005a99”, "backgroundAsset":null }' -H "Content-Type: application/json" -X POST https://pamforest.com/api/v1/data.php
